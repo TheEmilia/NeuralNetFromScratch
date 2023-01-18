487dda227587250eec75bf626bb279ea3d346d5f
TheEmilia <emiliaalexandrakiller@gmail.com>
2023-01-18 11:09:53 +0200

    first commit

---
+++ nnfs.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+// Neuron Code
+// Neurons are a function that returns the sum of a number of inputs, multiplied by their respective weights, and a bias
+// Takes a vector of input values, a vector of weight values corresponding to those input values, and a bias
+// Returns a single value of type double
+// Every neuron in a layer takes in the same inputs, but has different weights and bias, and thus returns a different value
