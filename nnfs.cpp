2a00b378ef69178d2fb48f8a548264fac2f4d215
TheEmilia <emiliaalexandrakiller@gmail.com>
2023-01-24 10:56:18 +0200

    introduced an error that prevents output printing

--- nnfs.cpp
+++ nnfs.cpp
@@ -13,29 +13,130 @@ using namespace std;
 // Every neuron in a layer takes in the same inputs, but has different weights and bias, and thus returns a different value
 
 // We know in advance the number of inputs and neurons per layer
-vector<double> inputs = {1, 2, 3, 2.5};
+vector<vector<double>> inputs = {{1, 2, 3, 2.5}, {2.0, 5.0, -1.0, 2.0}, {-1.5, 2.7, 3.3, -0.8}};
 vector<vector<double>> weights = {{0.2, 0.8, -0.5, 1.0}, {0.5, -0.91, 0.26, -0.5}, {-0.26, -0.27, 0.17, 0.87}};
 vector<double> biases = {2, 3, 0.5};
 
-const double neuron(const vector<double> &weights, const vector<double> &inputs, double bias)
+// Used to transpose weights before applying matrix multiplication in order to produce a correct output
+const vector<vector<double>> transposeMatrix(const vector<vector<double>> &input_matrix)
 {
-    return inner_product(weights.begin(), weights.end(), inputs.begin(), bias);
+    // Number of inputs = number of weights per neuron
+    // Number of neurons = number of batches of weights
+    // Weight default: weights(x axis) by batches(y axis)
+    // Transpose to batches(x) by weights(y)
+
+    // Dimensions of input matrix
+    int row_count = input_matrix.size();
+    int column_count = input_matrix[0].size();
+
+    // Check for suitable dimensions otherwise throw an error
+    if (row_count <= 0 || column_count <= 0)
+    {
+        cout << "Shape Error, check dimensions of transposed matrix" << endl;
+        return input_matrix;
+    }
+
+    // Create empty matrix of the correct dimensions
+    vector<vector<double>> transposed_matrix(column_count, vector<double>(row_count, 0));
+
+    // Swap rows with columns  and set value in transposed matrix
+    for (int i = 0; i < column_count; ++i)
+    {
+        for (int j = 0; j < row_count; ++j)
+        {
+            transposed_matrix[i][j] = input_matrix[j][i];
+        }
+    }
+
+    return transposed_matrix;
 }
 
-// Code adapted from https://stackoverflow.com/questions/61645527/how-can-i-do-a-dot-product-between-a-matrix-and-a-vector-in-c
-// passing vectors by reference avoids unnecessary copies
-vector<double> layer(const vector<vector<double>> &weights, const vector<double> &inputs, vector<double> &biases)
+// Acts as layer composed of inputs. matrix1 should be inputs, matrix2 should be transposed weights, and biases offset the calculated value for the corresponding neuron
+const vector<vector<double>> multiplyMatrices(const vector<vector<double>> &matrix_inputs, const vector<vector<double>> &matrix_weights, const vector<double> &biases)
 {
-    vector<double> results;
-    for (int i = 0; i < weights.size(); i++)
-        results.push_back(neuron(weights[i], inputs, biases[i]));
-    return results;
+    // Dimensions of inputs matrix
+    int row_count_inputs = matrix_inputs.size();
+    int column_count_inputs = matrix_inputs[0].size();
+
+    // Dimensions of weights matrix
+    int row_count_weights = matrix_weights.size();
+    int column_count_weights = matrix_weights[0].size();
+
+    // Check for suitable dimensions otherwise throw an error
+    if (row_count_inputs <= 0 || column_count_inputs <= 0 || column_count_weights <= 0 || row_count_weights <= 0)
+    {
+        cout << "Shape Error, check dimensions of transposed matrix" << endl;
+        return matrix_inputs;
+    }
+    if (column_count_inputs != row_count_weights)
+    {
+        cout << "Shape Error, check dimensions of transposed matrix" << endl;
+        return matrix_inputs;
+    }
+
+    // Create empty matrix of the correct dimensions
+    vector<vector<double>> output_matrix(row_count_inputs, vector<double>(column_count_weights, 0));
+
+    for (int i = 0; i < row_count_inputs; ++i)
+    {
+        for (int j = 0; j < column_count_inputs; ++j)
+        {
+            output_matrix[i][j] = biases[j];
+            cout << i << ", " << j << ", " << output_matrix[i][j] << endl;
+            for (int k = 0; k < column_count_weights; ++k)
+            {
+                output_matrix[i][j] += matrix_inputs[i][k] * matrix_weights[k][j]; // CHeck to ensure correct multiplication is occurring
+            }
+            cout << i << ", " << j << ", " << output_matrix[i][j] << endl;
+        }
+    }
+
+    return output_matrix;
+}
+
+// const vector<vector<double>> applyBiases(const vector<vector<double>> &matrix, const vector<double> &biases)
+// {
+//     // Dimensions of matrix - should always be square
+//     int side_length = matrix.size();
+
+//     vector<vector<double>> output_matrix = matrix;
+
+//     // traverses left to right then top to bottom
+//     for (int i = 0; i < side_length; ++i)
+//     {
+//         for (int j = 0; j < side_length; ++j)
+//         {
+//             output_matrix[i][j] += biases[j];
+//         }
+//     }
+
+//     return output_matrix;
+// }
+
+void printMatrix(vector<vector<double>> input_matrix)
+{
+
+    int sides = input_matrix.size();
+
+    for (int i = 0; i < sides; ++i)
+    {
+        for (int j = 0; j < sides; ++j)
+        {
+            cout << input_matrix[i][j] << " - ";
+        }
+        cout << endl;
+    }
 }
 
 int main()
 {
-    vector<double> output = layer(weights, inputs, biases);
+    const vector<vector<double>> transposed_weights = transposeMatrix(weights);
+    const vector<vector<double>> output = multiplyMatrices(inputs, transposed_weights, biases);
 
-    for (auto &value : output)
-        cout << value << endl;
+    // vector<vector<double>> output = applyBiases(output, biases);
+    // Biases still need to be applied
+    // doesn't print output values when run
+    cout << endl;
+    cout << "TEST" << endl;
+    // printMatrix(output);
 }
